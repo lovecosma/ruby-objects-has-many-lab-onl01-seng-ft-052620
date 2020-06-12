d64f4471102f08599b421e5112af0670b11a23d6
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-12 06:32:13 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,26 @@
 class Post
+
   attr_accessor :title
+
   def initialize(title)
+
     self.title = title
-  end
-  def author
 
   end
-  def author=(author)
-    
+
+  def author
+
+
+
   end
+
+
+
+  def author=(author)
+
+
+
+  end 
 
 
 end
