b601cf5bc11d9e37d163237cb58fc48449af225b
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-12 06:14:03 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+
+def initialize(name)
+self.name = name
+self.songs = []
+end
+
+
+ def add_song(song)
+   song.artist = self
+ end
+
+ def add_song_by_name(song_name)
+   @new_song = Song.new(song_name)
+   @new_song.artist=(self)
+   binding.pry
+ end
+
+ def self.song_count
+
+ end
+
+ end
---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :name, :artist
+  def initialize(name)
+    self.name = name
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    @artist.songs << self
+  end
+end
